81c694f16f5413d34fdd607b6cf6086a65f158ec
Anthony Ascencio <anthony2025@pm.me>
2020-08-06 00:42:53 -0400

    move functions around

---
+++ .config/fish/functions/backup_world.fish
@@ -0,0 +1,9 @@
+function backup_world
+  set today (date '+%m-%d-%Y')
+
+  set world_path rufus:/srv/forge/TheSlothKingdom
+  set backup_path /home/anthony/shared/saves/$today
+
+  mkdir -p $backup_path
+  scp -r $world_path $backup_path
+end
