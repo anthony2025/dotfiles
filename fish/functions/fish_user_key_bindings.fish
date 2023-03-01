332854c5851d911f8e1de4391fb4f04bbff7a0f7
Anthony Ascencio <anthony2025@pm.me>
2023-03-01 16:43:30 -0500

    remove unused plugin

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,20 +1,17 @@
 function fish_user_key_bindings --description "setup bindings automatically on startup"
-  # sets emacs-like keybindings first so vim ones win
-	fish_hybrid_key_bindings
+    # sets emacs keybindings first so vi ones win but common ones remain
+    fish_hybrid_key_bindings
 
-  # fzf mappings for normal mode
-	bind -M default \ct fzf_file_widget
-  bind -M default \cr fzf_history_widget
-  bind -M default \cp fzf_cd_widget
+    # fzf mappings for normal mode
+    bind -M default \ct fzf_file_widget
+    bind -M default \cr fzf_history_widget
+    bind -M default \cp fzf_cd_widget
 
-  # fzf mappings for insert mode
-  bind -M insert \ct fzf_file_widget
-  bind -M insert \cr fzf_history_widget
-  bind -M insert \cp fzf_cd_widget
+    # fzf mappings for insert mode
+    bind -M insert \ct fzf_file_widget
+    bind -M insert \cr fzf_history_widget
+    bind -M insert \cp fzf_cd_widget
 
-  # quickly change to insert mode from visual mode
-  bind -M visual -m insert i
-
-  # quickly exit insert mode to normal mode
-  bind -M insert jj "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char repaint-mode; end"
+    # quickly exit insert mode to normal mode
+    bind -M insert jj "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char repaint-mode; end"
 end
