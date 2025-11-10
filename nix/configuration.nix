bd119b304f97e5c1827b4972846510594cba549a
Anthony Ascencio <anthony2025@pm.me>
2025-11-09 20:45:46 -0500

    correct some warnings

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -5,11 +5,6 @@
 { config, pkgs, ... }:
 
 {
-  imports =
-    [
-      ./hardware-configuration.nix # Include the results of the hardware scan.
-    ];
-
   # Activate nix flakes
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
@@ -53,8 +48,8 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
--- nix/flake.nix
+++ nix/flake.nix
@@ -4,25 +4,21 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # home-manager = {
+    #   url = "github:nix-community/home-manager";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-
-      # Use "nixos", or your hostname as the name of the configuration
-      nixosConfigurations.glimmer = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs;};
-        modules = [
-          ./configuration.nix
-          inputs.home-manager.nixosModules.default
-        ];
-      };
+    # Use "nixos", or your hostname as the name of the configuration
+    nixosConfigurations.glimmer = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
+      modules = [
+        ./hardware-configuration.nix
+        ./configuration.nix
+        #inputs.home-manager.nixosModules.default
+      ];
+    };
   };
 }
