24e580a9a6bee703643c32e1db8411712ca3e84a
Anthony Ascencio <anthony2025@pm.me>
2025-12-16 00:04:00 -0500

    get rid of problematic bash file"

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -51,16 +51,6 @@
     # '')
   ];
 
-  programs.bash = {
-    enable = true;
-    shellAliases = {
-      t = "tmux";
-      tls = "tmux ls";
-      ta = "tmux a -t";
-      tn = "tmux new -t";
-    };
-  };
-
   # Home Manager can also manage your environment variables through
   # 'home.sessionVariables'. These will be explicitly sourced when using a
   # shell provided by Home Manager. If you don't want to manage your shell
