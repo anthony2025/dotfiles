cede375d02f59cc19f8309ddbc4c122747d3a005
Anthony Ascencio <anthony2025@pm.me>
2025-12-15 23:41:20 -0500

    correct hostnames

--- nix-on-droid/flake.nix
+++ nix-on-droid/flake.nix
@@ -12,7 +12,7 @@
 
   outputs = { self, nixpkgs, nix-on-droid }: {
 
-    nixOnDroidConfigurations.default = nix-on-droid.lib.nixOnDroidConfiguration {
+    nixOnDroidConfigurations.asuka = nix-on-droid.lib.nixOnDroidConfiguration {
       pkgs = import nixpkgs { system = "aarch64-linux"; };
       modules = [ ./nix-on-droid.nix ];
     };
